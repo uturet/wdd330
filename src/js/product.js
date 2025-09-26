6aa63380d60aac910ebda2d77427da45d78d3db6
cybenko.sergey <cybenko.sergey@gmail.com>
2025-09-25 21:06:32 -0500

    fixed add to cart function

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,14 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cart = getLocalStorage("so-cart");
+  if (Array.isArray(cart)) {
+    cart.push(product);
+    setLocalStorage("so-cart", cart);
+  } else {
+    setLocalStorage("so-cart", [product]);
+  }
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
