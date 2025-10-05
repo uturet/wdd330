cc7f22f671068e4f0426f9daab2ca90fbde82f54
cybenko.sergey <cybenko.sergey@gmail.com>
2025-10-04 18:24:40 -0600

    team 2 activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,22 +1,28 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { getParams } from "./utils.mjs";
+import productDetails, { addToCart } from "./productDetails.mjs";
 import { findProductById } from "./productData.mjs";
 
-function addProductToCart(product) {
-  const cart = getLocalStorage("so-cart");
-  if (Array.isArray(cart)) {
-    cart.push(product);
-    setLocalStorage("so-cart", cart);
-  } else {
-    setLocalStorage("so-cart", [product]);
-  }
-}
+// function addProductToCart(product) {
+//   const cart = getLocalStorage("so-cart");
+//   if (Array.isArray(cart)) {
+//     cart.push(product);
+//     setLocalStorage("so-cart", cart);
+//   } else {
+//     setLocalStorage("so-cart", [product]);
+//   }
+// }
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
+  addToCart(product);
 }
 
 // add listener to Add to Cart button
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+
+// Team Assignment 2
+const productId = getParams('product');
+console.log('does this run');
+productDetails(productId);
