e665901825b91009e48cb3676d2e53f2788872ee
cybenko.sergey <cybenko.sergey@gmail.com>
2025-10-16 15:10:21 -0600

    add default tent image and missingImage function in case real image is missing

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -3,20 +3,18 @@ import { renderListTemplate } from "./utils.mjs"
 
 function productCardTemplate(product) {
     return `<li class="product-card">
-        <a href="product_pages/index.html?product=${product.Id}">
-      <img
-      src="${product.Image}"
-      alt="Image of ${product.Name}"
-      />
-     <h3 class="card__brand">${product.Brand.Name}</h3>
-     <h2 class="card__name">${product.NameWithoutBrand}</h2>
-     <p class="product-card__price">$${product.FinalPrice}</p></a>
-     </li>`
+    <a href="product_pages/index.html?product=${product.Id}">
+        <img
+            onerror="missingImage(this)"
+            src="${product.Image}"
+            alt="Image of ${product.Name}" />
+    <h3 class="card__brand">${product.Brand.Name}</h3>
+    <h2 class="card__name">${product.NameWithoutBrand}</h2>
+    <p class="product-card__price">$${product.FinalPrice}</p></a>
+</li>`
 };
- 
 
-
-export default async function productList(selector, category ) {    
+export default async function productList(selector, category) {    
     const elements = document.querySelector(selector);
     const products = await getData(category);
     renderListTemplate(productCardTemplate, elements, products);
